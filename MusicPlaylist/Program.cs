d484787408a7142f55f1856c3c6932a6c6464333
Daniel Vang <daniel@danielvang.com>
2019-05-10 19:41:51 -0500

    First commit

---
+++ MusicPlaylist/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylist
+{
+    class Program
+    {
+        private static List<MusicPlaylist> MusicPlaylistList = new List<MusicPlaylist>();
+     
+        static void Main(string[] args)
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(',');
+
+            $"Row {lineNumber} contains {values.Length} values. It should contain {NumItemsInRow}."
+
+            var popSongsByYearDescending = from song in songs where song.Genre == "Pop" orderby song.Year descending select song;
+
+            foreach (var song in songs)
+            {
+                // do something with the song
+            }
+
+            foreach (Song song in popSongsByYearDescending)
+            {
+                report += song + "\n";
+            }
+
+
+        }
+    }
+}
